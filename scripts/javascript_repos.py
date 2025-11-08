f0b4470dfe0c057725267d60ee70cae19f6c4550
darlegui <arleguidaniel@gmail.com>
2025-11-07 19:21:43 -0800

    API github js

---
+++ scripts/javascript_repos.py
@@ -0,0 +1,53 @@
+import requests
+import plotly.express as px
+
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:javascript+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Convert the response object to a dictionary.
+response_dict = r.json()
+
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+# print(f"Repositories returned: {len(repo_dicts)}")
+# print("\nSelected information about each popular JavaScript repository:")
+
+repo_links, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts[:10]:
+    # Creating Hover
+    owner = repo_dict["owner"]["login"]
+    desc = repo_dict["description"]
+    hover_text = f"{owner}<br />{desc}<br />"
+    hover_text += f"Created: {repo_dict['created_at']}<br />"
+    hover_text += f"Updated: {repo_dict['updated_at']}<br />"
+
+    # Clickable links
+    full_name = repo_dict["full_name"]
+    repo_url = repo_dict["html_url"]
+    repo_link = f"<a href='{repo_url}'>{full_name}</a>"
+
+    # Appending
+    hover_texts.append(hover_text)
+    repo_links.append(repo_link)
+    stars.append(repo_dict["stargazers_count"])
+
+# Make visualization.
+title = "10 Most-Starred JavaScript Projects on GitHub"
+labels = {"x": "Repository", "y": "Stars"}
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
+
+fig.update_layout(
+    title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20
+)
+
+fig.update_traces(marker_color="SteelBlue", marker_opacity=0.6)
+
+fig.show()
